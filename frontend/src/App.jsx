a39502d8192e6424228e2c7fcf270b6c9ae31bad
ramza <ramzamohideen@gmail.com>
2025-02-09 11:21:11 +0530

    install reactjs and tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-center bg-slate-600 text-3xl font-bold underline">
+        App
+      </h1>
+    </>
+  );
+}
